ff36f21e141046db19665620850d38a511808275
PiggyAwesome <48888771+PiggyAwesome@users.noreply.github.com>
2022-07-03 15:47:02 +0200

    Create primefinder.py

---
+++ primefinder.py
@@ -0,0 +1,20 @@
+def do_thing(i):
+    for x in range(round(i/2)):
+            if x != i and x != 1 and x != 0:
+                try:
+                    if (i % x) == 0:
+                        return [False, x]
+                except: 
+                    pass
+    return [True, 0]
+
+
+check_until = 10000
+start_at = 100
+
+if __name__ == "__main__":
+    for i in range(check_until):
+        i += start_at
+        success, reason = do_thing(i)
+        if success:
+            print(f"{i}", end=" ")
